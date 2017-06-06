9831c5fd0c6579d9796289645c57b045d43ae26f
Tamela Lerma <tlerma83@gmail.com>
2017-06-06 15:14:09 -0500

    trying to receive data

--- app/controllers/BoardDetailCtrl.js
+++ app/controllers/BoardDetailCtrl.js
@@ -1,4 +1,16 @@
 "use strict";
-
+console.log("are we on speaking terms?");
 // getPins()
 // deletePins()
+app.controller("BoardDetailCtrl", function($q, $window, $location, $scope, DataFactory, AuthFactory){
+    let tempUID = 1234;
+    let userBoards = DataFactory.getUserBoards(tempUID);
+    console.log("what does data look like?", userBoards);
+
+    let userBoards = function() {
+
+    };
+
+    userBoards();
+
+});
