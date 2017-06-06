0dd076f9083d60bb907d44686c70f83e4ecdead7
Chaz Henricks <chazhenricks@gmail.com>
2017-06-06 08:39:42 -0500

    cleaned up template. Should be all good to start now

--- app/app.js
+++ app/app.js
@@ -6,3 +6,26 @@ console.log("HOWDY");
 const app = angular.module("Pinterrupted", ["ngRoute"]);
 
 
+app.config(function($routeProvider){
+    $routeProvider
+    .when('/', {
+        templateUrl: '',
+        controller: ''
+    })
+
+    .otherwise('/');
+});
+
+app.run(($location, FBCreds) =>{
+    let creds = FBCreds;
+    let AuthConfig = {
+        apiKey: creds.apiKey,
+        authDomain: creds.authDomain,
+        databaseURL : creds.databaseURL
+    };
+
+    firebase.initializeApp(AuthConfig);
+
+});
+
+
