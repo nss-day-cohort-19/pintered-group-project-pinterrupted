1fa7e4d9195d70b6721388fbd1c966a520115763
Madeline Power <madelinepower@Madelines-MacBook-Pro.local>
2017-06-06 21:25:16 -0500

    login and register works.. but FIRST needs the addUser function in DataFactory

--- app/controllers/AuthCtrl.js
+++ app/controllers/AuthCtrl.js
@@ -1,12 +1,56 @@
 "use strict";
 
-app.controller("AuthCtrl", function($scope, AuthFactory){
-    $scope.auth = {};
+//login, logout, register
 
-    $scope.registerUser = function(registerNewUser){
-        AuthFactory.registerWithEmail(registerNewUser)
-        .then((didRegister)=>{
-            console.log(didRegister);
-        });
+app.controller("AuthCtrl", function($scope, $window, $location, AuthFactory, DataFactory){
+    $scope.auth = {
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: ""
     };
+
+
+    let logout = () => {
+        console.log("NO ONE IS LOGGED IN");
+        AuthFactory.logout()
+          .then(function (data) {
+            $window.location.url = "#!/";
+          }, function (error) {
+            console.log("error occured on logout");
+          });
+      };
+
+      // when first loaded, make sure no one is logged in
+      if (AuthFactory.isAuthenticated()) {
+        logout();
+      }
+
+
+    $scope.registerUser = function(){
+        AuthFactory.registerWithEmail({
+          email: $scope.auth.email,
+          password: $scope.auth.password,
+        })
+        .then(function(user){
+          DataFactory.addUser({
+            uid: user.uid,
+            firstName: $scope.auth.firstName,
+            lastName: $scope.auth.lastName
+          })
+          .then((userData) => {
+            $scope.login();
+            $('#registerModal').modal('close');
+          }, (error) => {
+            console.log("Error creating user:", error);
+          });
+      });
+    };
+
+    $scope.login = function(){
+      AuthFactory.login($scope.auth)
+    .then(() => {
+      $window.location.href = "#!/explore";
+    });
+};
 });
