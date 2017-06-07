8fcb449b702e3540d3fe14946d73d1767d4b5cab
Tamela Lerma <tlerma83@gmail.com>
2017-06-06 19:53:09 -0500

    have new boards posting to database

--- app/controllers/BoardDetailCtrl.js
+++ app/controllers/BoardDetailCtrl.js
@@ -5,12 +5,22 @@ app.controller("BoardDetailCtrl", function($q, $window, $location, $scope, DataF
 
     $scope.tempUID = "1234";
     let user = AuthFactory.getUser();
-    console.log("Is there a current user?", user);
-    
+//    console.log("Is there a current user?", user);
+
+    $scope.newBoardObject = {};
+
     $scope.addNew = function (someText) {
         console.log("what did I get back?", someText);
+        $scope.newBoardObject.title = someText;
+        $scope.newBoardObject.uid = $scope.tempUID;
+        console.log("checking new board object", $scope.newBoardObject);
+        DataFactory.addBoard($scope.newBoardObject)
+        .then((newBoardSucces)=>{
+              console.log("Check firebase for new data", newBoardSucces);
+        });
     };
 
+    //sending user boards uid to get back board
     let userBoards = function () {
         $scope.boardArray = [];
         DataFactory.getUserBoards($scope.tempUID)
