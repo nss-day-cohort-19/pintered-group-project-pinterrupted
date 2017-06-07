5390a66ef873fd56358dca709241e86cb1dae61d
Tamela Lerma <tlerma83@gmail.com>
2017-06-06 20:48:15 -0500

    refactored code to pull back key from firebase to create unique ID's, modals for all cards are working. Delete from firebase almost there

--- app/controllers/BoardDetailCtrl.js
+++ app/controllers/BoardDetailCtrl.js
@@ -3,12 +3,16 @@
 
 app.controller("BoardDetailCtrl", function($q, $window, $location, $scope, DataFactory, AuthFactory){
 
+    //use for testing, delete once able to pull back current user UID
     $scope.tempUID = "1234";
+
+    //Should return UID to pass into both function on the page
     let user = AuthFactory.getUser();
 //    console.log("Is there a current user?", user);
 
     $scope.newBoardObject = {};
 
+    //pass into function input value from board-detail.html, push to boards collection in firebase and creates new custom key
     $scope.addNew = function (someText) {
         console.log("what did I get back?", someText);
         $scope.newBoardObject.title = someText;
@@ -20,15 +24,24 @@ app.controller("BoardDetailCtrl", function($q, $window, $location, $scope, DataF
         });
     };
 
-    //sending user boards uid to get back board
+    //delete button calls this function from board-detail.html
+    $scope.deleteBoard = function () {
+        console.log("delete button was clicked");
+    };
+
+
+
+    //sending userboards uid to get back board title and pass into board-detail.html
+    //use Object.keys to return custom key from Firebase, use this key to pass in a unique ID for //each modal that is created for each card in the ng-repeat
     let userBoards = function () {
         $scope.boardArray = [];
         DataFactory.getUserBoards($scope.tempUID)
         .then((boardObj)=>{
-            for (let boards in boardObj) {
-                let findCurrentUser = boardObj[boards];
-                $scope.boardArray.push(findCurrentUser.title);
-            }
+            Object.keys(boardObj).forEach( (key)=>{
+                boardObj[key].id = key;
+                $scope.boardArray.push(boardObj[key]);
+            });
+            console.log("Is the key coming", $scope.boardArray);
         });
     };
 
